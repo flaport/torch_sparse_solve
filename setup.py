3221591f34c0d41de9644279396db081e76395ae
flaport <floris.laporte@gmail.com>
2020-06-28 23:28:41 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ import glob
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
+import torch_sparse_solve
+
 conda = os.path.dirname(os.path.dirname(os.path.dirname(os.__file__)))
 folders = glob.glob(os.path.join(conda, "pkgs", "suitesparse-*"))
 folders = [folder for folder in folders if not folder.endswith(".conda")]
 suitesparse = sorted(folders)[-1]  # latest version if multiple installed.
 
-torch_sparse_solve = Extension(
+torch_sparse_solve_cpp = Extension(
     name="torch_sparse_solve_cpp",
     sources=["torch_sparse_solve.cpp"],
     include_dirs=[
@@ -34,6 +36,22 @@ torch_sparse_solve = Extension(
 
 setup(
     name="torch_sparse_solve_cpp",
-    ext_modules=[torch_sparse_solve],
+    version=torch_sparse_solve.__version__,
+    author=torch_sparse_solve.__author__,
+    author_email="floris.laporte@gmail.com",
+    description=torch_sparse_solve.__doc__,
+    long_description=open("readme.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/flaport/torch_sparse_solve",
+    py_modules=["torch_sparse_solve"],
+    ext_modules=[torch_sparse_solve_cpp],
     cmdclass={"build_ext": cpp_extension.BuildExtension},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
 )
